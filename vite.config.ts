f7f80426e23f0866831d8131097bc10b9d5d6571
hmatsu47 <hmatsu47@gmail.com>
2022-12-08 22:06:07 +0900

    vite.config.ts でdev の起動ポート修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solidPlugin()],
   build: {
-    target: 'esnext',
-    polyfillDynamicImport: false,
+    target: "esnext",
+    // polyfillDynamicImport: false,
+  },
+  server: {
+    port: 3000,
   },
 });
